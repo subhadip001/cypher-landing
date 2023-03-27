0c3da076562a246242ed6e1ba813c52cee78a8c1
Subhadip <mnhacker2001@gmail.com>
2023-03-27 19:18:25 +0530

    update

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,18 +2,20 @@ import React from "react";
 import linkedIn from "../../assets/linkedin.png";
 import mailIcon from "../../assets/mail.png";
 
-
 const Footer = () => {
   return (
     <div id="contact" className="">
       <hr />
       <footer className="flex justify-between w-[87%] m-auto h-[124px] items-center">
         <div className="left flex gap-8">
-          <a href="#" target="_blank">
-            <img src={linkedIn} alt="" height="36px" width="36px" />
+          <a
+            href="https://www.linkedin.com/company/92510561/admin/?feedType=following"
+            target="_blank"
+          >
+            <img src={linkedIn} alt="linkedin" height="36px" width="36px" />
           </a>
           <a href="mailto:cypherappservice@cypher.org.in" target="_blank">
-            <img src={mailIcon} alt="" height="36px" width="36px" />
+            <img src={mailIcon} alt="mail" height="36px" width="36px" />
           </a>
         </div>
         <div className="right ">
--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useLocation } from "react-router-dom";
-import logo from "../../assets/cypher-trans.png"
+import logo from "../../assets/cypher-trans.png";
 
 const NavBar = () => {
-    const location = useLocation()
+  const location = useLocation();
   return (
     <>
       <div className="flex items-center justify-center h-[100px] sticky z-50 top-0">
@@ -12,14 +12,20 @@ const NavBar = () => {
           <div className="brand flex justify-between items-center text-white gap-3">
             <img className="block w-[3rem] h-[3rem]" src={logo} alt="cypher" />
             <Link to="/">
-              <span className="text-2xl font-semibold">CYPHER BOOSTER</span>
+              <span className="text-2xl font-semibold">CYPHER</span>
             </Link>
           </div>
           <nav className="nav text-white flex items-center gap-14">
-            <Link className={location.pathname === "/about" && "font-semibold"} to="/about">
+            <Link
+              className={location.pathname === "/about" && "font-semibold"}
+              to="/about"
+            >
               <span> About</span>
             </Link>
-            <a href="#contact" className={location.pathname === "/#contact" && "font-semibold"}>
+            <a
+              href="#contact"
+              className={location.pathname === "/#contact" && "font-semibold"}
+            >
               <span> Contact Us</span>
             </a>
           </nav>
